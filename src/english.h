23ed01d78e1d2ac23b0f9e28edef79693c16e4b4
rheeze <mauro.bazzano@gmail.com>
2014-01-04 16:12:54 +0100

    Minor fixes, add english language

---
+++ src/english.h
@@ -0,0 +1,25 @@
+static const char* const DOWS_SHORT[] = {
+  "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" 
+};
+
+static const char* const MONTHS[] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" 
+};
+
+/*
+static const char* const DOWS[] = {
+  "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" 
+};
+
+static const char* const ONES[] = {
+  "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"
+};
+
+static const char* const TEENS[] = {
+  "TEN", "ELEVEN", "TUELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN"
+};
+
+static const char* const TENS[] = {
+  "TWENTY", "THIRTY", "FORTY", "FIFTY"
+};
+*/
---
+++ src/italian.h
@@ -0,0 +1,25 @@
+static const char* const DOWS_SHORT[] = {
+  "Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab" 
+};
+
+static const char* const MONTHS[] = {
+  "Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic" 
+};
+
+/*
+static const char* const DOWS[] = {
+  "DOMENICA", "LUNEDÌ", "MARTEDÌ", "MERCOLEDÌ", "GIOVEDÌ", "VENERDÌ", "SABATO" 
+};
+
+static const char* const ONES[] = {
+  "ZERO", "UNO", "DUE", "TRE", "QUATTRO", "CINQUE", "SEI", "SETTE", "OTTO", "NOVE"
+};
+
+static const char* const TEENS[] = {
+  "DIECI", "UNDICI", "DODICI", "TREDICI", "QUATTORDICI", "QUINDICI", "SEDICI", "DICIASSETTE", "DICIOTTO", "DICIANNOVE"
+};
+
+static const char* const TENS[] = {
+  "VENTI", "TRENTA", "QUARANTA", "CINQUANTA"
+};
+*/
